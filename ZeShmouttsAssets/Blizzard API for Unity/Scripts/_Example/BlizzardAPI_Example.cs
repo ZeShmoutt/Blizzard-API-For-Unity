fa1b209fd3eacfd8b2d3532d393907fb5d056f87
ZeShmoutt <36408045+ZeShmoutt@users.noreply.github.com>
2020-04-07 21:16:04 +0200

    Added example and removed the temporary singleton

---
+++ ZeShmouttsAssets/Blizzard API for Unity/Scripts/_Example/BlizzardAPI_Example.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using ZeShmouttsAssets.BlizzardAPI;
+using ZeShmouttsAssets.BlizzardAPI.JSON;
+
+public class BlizzardAPI_Example : MonoBehaviour
+{
+	void Start()
+	{
+		//Retrieve informations about the World of Warcraft achievement "The Loremaster" (https://www.wowhead.com/achievement=7520/)
+		StartCoroutine(BlizzardAPI_WowGameData.CGetAchievement(API.BattleNetRegion.UnitedStates, 7520, DebugMessage));
+	}
+
+	void DebugMessage(WoWAchievement_JSON achievement)
+	{
+		Debug.LogFormat("Name = '{0}' ; Description = '{1}' ; Value = {2} points", achievement.name.en_US, achievement.description.en_US, achievement.points);
+	}
+}
