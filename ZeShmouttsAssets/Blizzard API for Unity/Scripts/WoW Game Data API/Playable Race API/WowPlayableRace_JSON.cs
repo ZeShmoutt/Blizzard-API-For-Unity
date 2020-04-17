64999d3da0a834061b287e65c6108cafdafbf509
ZeShmoutt <36408045+ZeShmoutt@users.noreply.github.com>
2020-04-17 16:32:59 +0200

    Reorganized the folders

---
+++ ZeShmouttsAssets/Blizzard API for Unity/Scripts/WoW Game Data API/Playable Race API/WowPlayableRace_JSON.cs
@@ -0,0 +1,18 @@
+﻿namespace ZeShmouttsAssets.BlizzardAPI.JSON
+{
+	/// <summary>
+	/// JSON structure for an index of World of Warcraft playable races.
+	/// </summary>
+	[System.Serializable]
+	public class WowPlayableRace_JSON : Object_Json
+	{
+		public LinkStruct _links;
+
+		public int id;
+		public LocalizedString name;
+		public GenderName gender_name;
+		public TypeNameStruct faction;
+		public bool is_selectable;
+		public bool is_allied_race;
+	}
+}
---
+++ ZeShmouttsAssets/Blizzard API for Unity/Scripts/WoW Game Data API/Playable Race API/WowPlayableRacesIndex_JSON.cs
@@ -0,0 +1,13 @@
+﻿namespace ZeShmouttsAssets.BlizzardAPI.JSON
+{
+	/// <summary>
+	/// JSON structure for an index of World of Warcraft playable races.
+	/// </summary>
+	[System.Serializable]
+	public class WowPlayableRacesIndex_JSON : Object_Json
+	{
+		public LinkStruct _links;
+
+		public RefNameIdStruct[] races;
+	}
+}
