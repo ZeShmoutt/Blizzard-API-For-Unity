bbc73b97a0acfa4de1353d42ff084a699c90341c
ZeShmoutt <36408045+ZeShmoutt@users.noreply.github.com>
2020-04-28 17:09:10 +0200

    Added WoW Game Data endpoints support : Title, WoW Token

---
+++ ZeShmouttsAssets/Blizzard API for Unity/Scripts/WoW Game Data API/WoW Token API/Endpoint_WoW_GameData_WoWTokenIndex.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using ZeShmouttsAssets.BlizzardAPI.JSON;
+
+namespace ZeShmouttsAssets.BlizzardAPI
+{
+	/// <summary>
+	/// Interface for working with the Blizzard API inside Unity.
+	/// </summary>
+	public static partial class BlizzardAPI
+	{
+		/// <summary>
+		/// API endpoints related to World of Warcraft game data (items, spells, etc.).
+		/// Reference : https://develop.battle.net/documentation/world-of-warcraft/game-data-apis
+		/// </summary>
+		public static partial class WowGameData
+		{
+			/// <summary>
+			/// Coroutine that retrieves the WoW Token index of a specific region.
+			/// </summary>
+			/// <param name="region">The region of the data to retrieve.</param>
+			/// <param name="action_Result">Action to execute with the data once retrieved and converted.</param>
+			/// <param name="ifModifiedSince">Adds a request header to check if the document has been modified since this date (in HTML format), which will return an empty response body if it's older.</param>
+			/// <param name="action_LastModified">Action to execute with the date of the last server-side modification to the document.</param>
+			/// <returns></returns>
+			public static IEnumerator GetWoWTokenIndex(BattleNetRegion region, Action<WowWoWTokenIndex_JSON> action_Result, string ifModifiedSince = null, Action<string> action_LastModified = null)
+			{
+				string path = "/data/wow/token/index";
+				yield return SendRequest(region, namespaceDynamic, path, action_Result, ifModifiedSince, action_LastModified);
+			}
+		}
+	}
+}
+
+namespace ZeShmouttsAssets.BlizzardAPI.JSON
+{
+	/// <summary>
+	/// JSON structure for a WoW Token index.
+	/// </summary>
+	[Serializable]
+	public class WowWoWTokenIndex_JSON : Object_Json
+	{
+		public LinkStruct _links;
+
+		public long last_updated_timestamp;
+		public long price;
+	}
+}
