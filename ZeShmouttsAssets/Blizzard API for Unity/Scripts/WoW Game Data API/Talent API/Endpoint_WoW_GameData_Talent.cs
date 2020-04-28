5be16f33123735304699d0313c49abdbb9bb3a04
ZeShmoutt <36408045+ZeShmoutt@users.noreply.github.com>
2020-04-28 16:54:08 +0200

    Added WoW Game Data endpoint support : Talent

---
+++ ZeShmouttsAssets/Blizzard API for Unity/Scripts/WoW Game Data API/Talent API/Endpoint_WoW_GameData_Talent.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using ZeShmouttsAssets.BlizzardAPI.JSON;
+
+namespace ZeShmouttsAssets.BlizzardAPI
+{
+	/// <summary>
+	/// Interface for working with the Blizzard API inside Unity.
+	/// </summary>
+	public static partial class BlizzardAPI
+	{
+		/// <summary>
+		/// API endpoints related to World of Warcraft game data (items, spells, etc.).
+		/// Reference : https://develop.battle.net/documentation/world-of-warcraft/game-data-apis
+		/// </summary>
+		public static partial class WowGameData
+		{
+			/// <summary>
+			/// Coroutine that retrieves a WoW talent by ID.
+			/// </summary>
+			/// <param name="talentId">The ID of the talent.</param>
+			/// <param name="action_Result">Action to execute with the data once retrieved and converted.</param>
+			/// <param name="ifModifiedSince">Adds a request header to check if the document has been modified since this date (in HTML format), which will return an empty response body if it's older.</param>
+			/// <param name="action_LastModified">Action to execute with the date of the last server-side modification to the document.</param>
+			/// <param name="region">The region of the data to retrieve.</param>
+			/// <returns></returns>
+			public static IEnumerator GetTalent(int talentId, Action<WowTalent_JSON> action_Result, string ifModifiedSince = null, Action<string> action_LastModified = null, BattleNetRegion region = DefaultRegion)
+			{
+				string path = string.Format("/data/wow/talent/{0}", talentId);
+				yield return SendRequest(region, namespaceStatic, path, action_Result, ifModifiedSince, action_LastModified);
+			}
+		}
+	}
+}
+
+namespace ZeShmouttsAssets.BlizzardAPI.JSON
+{
+	/// <summary>
+	/// JSON structure for World of Warcraft talents.
+	/// </summary>
+	[Serializable]
+	public class WowTalent_JSON : Object_Json
+	{
+		public LinkStruct _links;
+
+		public int id;
+		public int tier_index;
+		public int column_index;
+		public int level;
+		public LocalizedString description;
+		public RefNameIdStruct spell;
+		public RefNameIdStruct playable_class;
+		public RefNameIdStruct playable_specialization;
+		public RefNameIdStruct overrides_spell;
+	}
+}
