5a2dd56e5a5dd7840725ec7333fcf358603d5bb1
ZeShmoutt <36408045+ZeShmoutt@users.noreply.github.com>
2020-04-28 16:28:42 +0200

    Added WoW Game Data endpoints support : Region, Reputations

---
+++ ZeShmouttsAssets/Blizzard API for Unity/Scripts/WoW Game Data API/Region API/Endpoint_WoW_GameData_Region.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using ZeShmouttsAssets.BlizzardAPI.JSON;
+
+namespace ZeShmouttsAssets.BlizzardAPI
+{
+	/// <summary>
+	/// Interface for working with the Blizzard API inside Unity.
+	/// </summary>
+	public static partial class BlizzardAPI
+	{
+		/// <summary>
+		/// API endpoints related to World of Warcraft game data (items, spells, etc.).
+		/// Reference : https://develop.battle.net/documentation/world-of-warcraft/game-data-apis
+		/// </summary>
+		public static partial class WowGameData
+		{
+			/// <summary>
+			/// Coroutine that retrieves a WoW region by ID.
+			/// </summary>
+			/// <param name="regionId">The ID of the region.</param>
+			/// <param name="action_Result">Action to execute with the data once retrieved and converted.</param>
+			/// <param name="ifModifiedSince">Adds a request header to check if the document has been modified since this date (in HTML format), which will return an empty response body if it's older.</param>
+			/// <param name="action_LastModified">Action to execute with the date of the last server-side modification to the document.</param>
+			/// <param name="region">The region of the data to retrieve.</param>
+			/// <returns></returns>
+			public static IEnumerator GetRegion(int regionId, Action<WowRegion_JSON> action_Result, string ifModifiedSince = null, Action<string> action_LastModified = null, BattleNetRegion region = DefaultRegion)
+			{
+				string path = string.Format("/data/wow/region/{0}", regionId);
+				yield return SendRequest(region, namespaceDynamic, path, action_Result, ifModifiedSince, action_LastModified);
+			}
+		}
+	}
+}
+
+namespace ZeShmouttsAssets.BlizzardAPI.JSON
+{
+	/// <summary>
+	/// JSON structure for World of Warcraft regions.
+	/// </summary>
+	[Serializable]
+	public class WowRegion_JSON : Object_Json
+	{
+		public LinkStruct _links;
+
+		public int id;
+		public LocalizedString name;
+		public string tag;
+	}
+}
